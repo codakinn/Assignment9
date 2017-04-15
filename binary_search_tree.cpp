62cb233e8befddc37248194ff17794ccc572e023
codakinn <codakinn@ut.utm.edu>
2017-04-15 15:50:52 -0500

    Seventh commit: updating BST implementation

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -26,9 +26,41 @@ void BinarySearchTree::destructorHelper(Node *current)
 void BinarySearchTree::set(string key, int value)
 {
   if(root == NULL){
-    root = new Node(key);
-    
+    root = new Node(key, value);    
   }
   else
     insertHelper(root, key, value);
+}
+
+void BinarySearchTree::insertHelper(Node* current, string key, int value)
+{
+   if(value < current -> data) //Insert Left
+   {
+       if(current -> left == NULL)
+         current -> left = new Node(key, value);
+       else
+         insertHelper(current -> left, key, value);
+   }
+   else //Insert Right
+   {
+     if(current -> right == NULL)
+       current -> right == new Node(key, value)
+     else
+       insertHelper(current -> right, key, value);
+   }
+}
+
+int BinarySearchTree::find(string key)
+{
+  findHelper(root, key);
+}
+
+int BinarySearchTree::findHelper(Node *current, string key)
+{
+  if(current -> key == key)
+    return current -> data;
+  else if(current -> key < key)
+    findHelper(current -> right, key);
+  else if(current -> key > key)
+    findHelper(current -> left, key);
 }
